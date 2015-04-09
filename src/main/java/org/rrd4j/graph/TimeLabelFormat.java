b98ebeb2c86a8b732afc89602f67e220cadd4402
Brian Harrington <brharrington@gmail.com>
2015-04-08 21:09:55 -0700

    fix #64, allow more control over time labels
    Same as the patch from that ticket with some javadocs added
    and a unit test for SimpleTimeLabelFormat.

---
+++ src/main/java/org/rrd4j/graph/TimeLabelFormat.java
@@ -0,0 +1,21 @@
+package org.rrd4j.graph;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Simplified version of DateFormat for just defining how to map a timestamp into a label for
+ * presentation.
+ */
+public interface TimeLabelFormat {
+    /**
+     * Format a timestamp.
+     *
+     * @param calendar   calendar to use for the formatter
+     * @param locale     locale that will be used with {@code String.format}
+     * @param timestamp  instant to format
+     * @return           formatted string for the timestamp
+     */
+    String format(Calendar calendar, Locale locale, Date timestamp);
+}
