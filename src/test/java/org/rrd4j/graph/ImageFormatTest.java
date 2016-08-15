bc783c6063c2e69231f5088617482e67af531c54
Fabrice Bacchella <fbacchella@gmail.com>
2016-08-15 12:56:44 +0200

    Wrong junit import in a test.

--- src/test/java/org/rrd4j/graph/ImageFormatTest.java
+++ src/test/java/org/rrd4j/graph/ImageFormatTest.java
@@ -3,8 +3,7 @@ package org.rrd4j.graph;
 import java.awt.Color;
 import java.io.IOException;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.rrd4j.ConsolFun;
 
