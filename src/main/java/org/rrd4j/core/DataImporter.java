9a965dac463a4dce7a177ddb989f61bffa365cfc
Fabrice Bacchella <fbacchella@gmail.com>
2019-01-27 19:17:05 +0100

    DataImporter is now public, for custom imports.

--- src/main/java/org/rrd4j/core/DataImporter.java
+++ src/main/java/org/rrd4j/core/DataImporter.java
@@ -6,54 +6,59 @@ import java.io.IOException;
 import org.rrd4j.ConsolFun;
 import org.rrd4j.DsType;
 
-abstract class DataImporter implements Closeable {
+/**
+ * <p>An abstract class to import data from external source.</p>
+ * @author Fabrice Bacchella
+ * @since 3.5
+ */
+public abstract class DataImporter implements Closeable {
 
     // header
-    abstract String getVersion() throws IOException;
+    abstract public String getVersion() throws IOException;
 
-    abstract long getLastUpdateTime() throws IOException;
+    abstract public long getLastUpdateTime() throws IOException;
 
-    abstract long getStep() throws IOException;
+    abstract public long getStep() throws IOException;
 
-    abstract int getDsCount() throws IOException;
+    abstract public int getDsCount() throws IOException;
 
-    abstract int getArcCount() throws IOException;
+    abstract public int getArcCount() throws IOException;
 
     // datasource
-    abstract String getDsName(int dsIndex) throws IOException;
+    abstract public String getDsName(int dsIndex) throws IOException;
 
-    abstract DsType getDsType(int dsIndex) throws IOException;
+    abstract public DsType getDsType(int dsIndex) throws IOException;
 
-    abstract long getHeartbeat(int dsIndex) throws IOException;
+    abstract public long getHeartbeat(int dsIndex) throws IOException;
 
-    abstract double getMinValue(int dsIndex) throws IOException;
+    abstract public double getMinValue(int dsIndex) throws IOException;
 
-    abstract double getMaxValue(int dsIndex) throws IOException;
+    abstract public double getMaxValue(int dsIndex) throws IOException;
 
     // datasource state
-    abstract double getLastValue(int dsIndex) throws IOException;
+    abstract public double getLastValue(int dsIndex) throws IOException;
 
-    abstract double getAccumValue(int dsIndex) throws IOException;
+    abstract public double getAccumValue(int dsIndex) throws IOException;
 
-    abstract long getNanSeconds(int dsIndex) throws IOException;
+    abstract public long getNanSeconds(int dsIndex) throws IOException;
 
     // archive
-    abstract ConsolFun getConsolFun(int arcIndex) throws IOException;
+    abstract public ConsolFun getConsolFun(int arcIndex) throws IOException;
 
-    abstract double getXff(int arcIndex) throws IOException;
+    abstract public double getXff(int arcIndex) throws IOException;
 
-    abstract int getSteps(int arcIndex) throws IOException;
+    abstract public int getSteps(int arcIndex) throws IOException;
 
-    abstract int getRows(int arcIndex) throws IOException;
+    abstract public int getRows(int arcIndex) throws IOException;
 
     // archive state
-    abstract double getStateAccumValue(int arcIndex, int dsIndex) throws IOException;
+    abstract public double getStateAccumValue(int arcIndex, int dsIndex) throws IOException;
 
-    abstract int getStateNanSteps(int arcIndex, int dsIndex) throws IOException;
+    abstract public int getStateNanSteps(int arcIndex, int dsIndex) throws IOException;
 
-    abstract double[] getValues(int arcIndex, int dsIndex) throws IOException;
+    abstract public double[] getValues(int arcIndex, int dsIndex) throws IOException;
 
-    long getEstimatedSize() throws IOException {
+    protected long getEstimatedSize() throws IOException {
         int dsCount = getDsCount();
         int arcCount = getArcCount();
         int rowCount = 0;
