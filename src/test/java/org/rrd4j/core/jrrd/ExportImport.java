1c8e4eba1477dedafbd61969ab6a79811ecdf281
Fabrice Bacchella <fbacchella@gmail.com>
2019-01-27 23:53:52 +0100

    Adding a new test.

--- src/test/java/org/rrd4j/core/jrrd/ExportImport.java
+++ src/test/java/org/rrd4j/core/jrrd/ExportImport.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.PrintStream;
 import java.net.URL;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.rrd4j.core.RrdDb;
+import org.rrd4j.core.RrdNioBackendFactory;
 
 public class ExportImport {
 
@@ -17,8 +19,7 @@ public class ExportImport {
     @Test
     public void testExport() throws Exception {
         // create xml file and write into it all rrd data
-        //File rrdXmlFile = testFolder.newFile("testexport.xml");
-        File rrdXmlFile = new File("/tmp/testexport.xml");
+        File rrdXmlFile = testFolder.newFile("testexport.xml");
 
         URL url = getClass().getResource("/rrdtool/0003l648.rrd"); 
 
@@ -26,8 +27,28 @@ public class ExportImport {
         rrd.toXml(new PrintStream(rrdXmlFile));
         rrd.close();
 
+        String imported = testFolder.getRoot().getAbsolutePath() + "/testexport.rrd";
         // create rrd4j database from the xml file created previously
-        RrdDb.getBuilder().setPath(testFolder.newFile("testexport.rrd").toURI()).setExternalPath(rrdXmlFile.getCanonicalPath()).doimport();
+        RrdDb.getBuilder().setPath(imported).setExternalPath(rrdXmlFile.getCanonicalPath()).doimport();
+        
+        try (RrdDb db = RrdDb.getBuilder().setPath(imported).setReadOnly().setBackendFactory(new RrdNioBackendFactory()).build()) {
+            Assert.assertNotNull(db.getDatasource("speed"));
+            Assert.assertNotNull(db.getDatasource("weight"));
+        }
+    }
+
+    @Test
+    public void testImport() throws Exception {
+        URL url = getClass().getResource("/rrdtool/0003l648.rrd"); 
+
+        String imported = testFolder.getRoot().getAbsolutePath() + "/testexport.rrd";
+        // create rrd4j database from the xml file created previously
+        RrdDb.getBuilder().setPath(imported).setRrdToolImporter(url.getFile()).doimport();
+
+        try (RrdDb db = RrdDb.getBuilder().setPath(imported).setReadOnly().setBackendFactory(new RrdNioBackendFactory()).build()) {
+            Assert.assertNotNull(db.getDatasource("speed"));
+            Assert.assertNotNull(db.getDatasource("weight"));
+        }
     }
 
 }
