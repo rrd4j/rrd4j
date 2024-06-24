645d03a268d6d34c601e252658d35bf5fb673ccb
Scott Lipcon <scott.lipcon@focusedsupport.com>
2024-06-24 10:00:28 -0400

    Upgrade to mongodb-java-sync 4.11

--- src/test/java/org/rrd4j/core/RrdDbMongoDbTest.java
+++ src/test/java/org/rrd4j/core/RrdDbMongoDbTest.java
@@ -2,7 +2,10 @@ package org.rrd4j.core;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -11,8 +14,7 @@ import org.rrd4j.ConsolFun;
 import org.rrd4j.DsType;
 
 import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoClient;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -33,11 +35,14 @@ public class RrdDbMongoDbTest {
 
     @Test
     public void testLifeCycle() throws IOException {
-        try (MongoClient mongoClient = new MongoClient(Arrays.asList(new ServerAddress("localhost")),
-                new MongoClientOptions.Builder()
-                .serverSelectionTimeout(2000)
-                .minConnectionsPerHost(0)
-                .build())) {
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings( builder -> builder.hosts( Arrays.asList( new ServerAddress( "localhost" ) ) ) )
+                .applyToClusterSettings( builder -> builder.serverSelectionTimeout(2000, TimeUnit.MILLISECONDS))
+                .applyToConnectionPoolSettings( builder -> builder.minSize(0) )
+                .build();
+
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
             MongoDatabase mongodb = mongoClient.getDatabase("mydb");
             MongoCollection<DBObject> collection = mongodb.getCollection("test", DBObject.class);
             RrdBackendFactory factory = new RrdMongoDBBackendFactory(mongoClient, collection, false);
